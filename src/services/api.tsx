aa8938ded4cd00d7c810e32b4f417c10c3b03c01
Ariel Tanami <arieltanami122@gmail.com>
2025-09-04 19:14:15 +0300

    Update API_URL to use production endpoint

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://riddles-3617439db6e9.herokuapp.com/";
 
 const api = axios.create({
   baseURL: API_URL,
